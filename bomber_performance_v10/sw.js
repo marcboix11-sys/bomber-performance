ffed4a9cbcd6f6618c1c202d47bf964ca1b3d8aa
marcboix11-sys <marcboix11@gmail.com>
2026-09-26 13:54:53 +0200

    Add files via upload

---
+++ bomber_performance_v10/sw.js
@@ -0,0 +1,5 @@
+const CACHE='bomber-performance-v10';
+const CORE=['./','./index.html','./manifest.json','./icons/icon-192.png','./icons/icon-512.png'];
+self.addEventListener('install',e=>{e.waitUntil(caches.open(CACHE).then(c=>c.addAll(CORE)).then(()=>self.skipWaiting()))});
+self.addEventListener('activate',e=>{e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim()))});
+self.addEventListener('fetch',e=>{if(e.request.method!=='GET')return;const u=new URL(e.request.url);const navigation=e.request.mode==='navigate'||u.pathname.endsWith('/index.html');e.respondWith((navigation?fetch(e.request).then(res=>{const copy=res.clone();caches.open(CACHE).then(c=>c.put('./index.html',copy));return res}).catch(()=>caches.match('./index.html')):caches.match(e.request).then(cached=>cached||fetch(e.request).then(res=>{const copy=res.clone();if(u.origin===self.location.origin)caches.open(CACHE).then(c=>c.put(e.request,copy));return res}).catch(()=>cached))));});
